db7aa84c9fc554a4f9d249735561df7cc144e4f9
Juniarto <juniarto.saputra@framecad.com>
2025-03-16 12:01:55 +1300

    No secrets

--- examples/stream/chatapi.h
+++ examples/stream/chatapi.h
@@ -2,9 +2,6 @@
 #include <curl/curl.h>
 #include <nlohmann/json.hpp>
 
-const std::string OPENAI_API_KEY = "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";
-const std::string DEEPSEEK_API_KEY = "sk-e721f7e4fe834b969e1ea5113f0e3d3f";
-
 class ChatAPI {
 private:
     std::string api_key;
